bf9bb10d6c000c9ae536dbe38b17cd8a41e54cfd
Martin Raunkjær Andersen <mra@raunsoft.dk>
2026-08-12 16:03:13 +0200

    VersusBigRotator

---
+++ Scenes/VersusBigRotator/VersusBigRotatorScene.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using BallDuel.Scenes.Shared;
+using BallDuel.scripts;
+using Godot;
+
+namespace BallDuel.Scenes.VersusBigRotator;
+
+public partial class VersusBigRotatorScene : BaseScene
+{
+    public override void _Ready()
+    {
+        base._Ready();
+
+        BlockingMessageController.Init(this);
+        CountdownController.Init(this);
+        CountdownController.StartCountdown();
+
+        Border.CollisionCallback = body =>
+        {
+            if (body is PlayerBall ball)
+            {
+                Console.WriteLine("ball: " + ball.ControllerId + " touched border");
+                GetTree().CreateTimer(0.1).Timeout += CheckForWin;
+            }
+        };
+    }
+
+    private void CheckForWin()
+    {
+        Console.WriteLine("Checking for win...");
+        
+        // TODO: Check for score win
+
+        List<PlayerBall> remainingPlayerList =
+            playerBallList.Where(b => b.IsControllerConnected() && Math.Abs(b.Position.X) < 50000).ToList();
+
+        if (remainingPlayerList.Count == 1)
+        {
+            PlayerBall remainingPlayer = remainingPlayerList[0];
+            var scoreLabel = GetNode<RichTextLabel>("Player" + (remainingPlayer.ControllerId + 1) + "Score");
+            var oldScore = int.Parse(scoreLabel.Text);
+            Console.WriteLine(
+                $"player {remainingPlayer.ControllerId} wins, old score: {oldScore} new score: {oldScore + 1}");
+            scoreLabel.Text = (oldScore + 1).ToString();
+            StartNextRound();
+        }
+        else if (remainingPlayerList.Count == 0)
+        {
+            Console.WriteLine("draw");
+            StartNextRound();
+        }
+        else
+        {
+            Console.WriteLine(remainingPlayerList.Count + " players left");
+        }
+    }
+
+    private void StartNextRound()
+    {
+        ResetPositions();
+        GetNode<MiddleSpinnyThing>("MiddleSpinnyThing").Reset();
+        CountdownController.StartCountdown();
+    }
+}
