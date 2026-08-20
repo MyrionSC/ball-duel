967afe655f834d935dec94fe0276f7cf9d48ed70
Martin Raunkjær Andersen <mra@raunsoft.dk>
2026-08-20 14:06:49 +0200

    create GoldenBallWithObstaclesScene

---
+++ Scenes/GoldenBallWithObstacles/GoldenBallWithObstaclesScene.cs
@@ -0,0 +1,48 @@
+using BallDuel.Scenes.Shared;
+using BallDuel.scripts;
+using Godot;
+
+namespace BallDuel.Scenes.GoldenBallWithObstacles;
+
+public partial class GoldenBallWithObstaclesScene : BaseScene
+{
+    public override void _Ready()
+    {
+        base._Ready();
+
+        BlockingMessageController.Init(this);
+
+        CountdownController.Init(this);
+        CountdownController.StartCountdown();
+    }
+
+    public override void ResetScene()
+    {
+        base.ResetScene();
+        GetNode<Ball>("GoldenBall").ResetToStart();
+    }
+
+    public void BallEnteredPlayer1Goal(Node2D body) => UpdateScoreAndResetGoldenBall(body, "Blue");
+    public void BallEnteredPlayer2Goal(Node2D body) => UpdateScoreAndResetGoldenBall(body, "Red");
+    public void BallEnteredPlayer3Goal(Node2D body) => UpdateScoreAndResetGoldenBall(body, "Green");
+    public void BallEnteredPlayer4Goal(Node2D body) => UpdateScoreAndResetGoldenBall(body, "Yellow");
+
+    private void UpdateScoreAndResetGoldenBall(Node2D body, NodePath colorName)
+    {
+        if (body is Ball ball && ball.GetName().ToString() == "GoldenBall")
+        {
+            var scoreNode = GetNode<RichTextLabel>($"{colorName}Score");
+            var newScore = int.Parse(scoreNode.Text) + 1;
+            scoreNode.Text = newScore.ToString();
+
+            ball.ResetToStart();
+
+            if (newScore >= 5)
+            {
+                Globals.InputDisabled = true;
+                BlockingMessageController.ShowBlockingMessage($"{colorName} wins!");
+            }
+        }
+    }
+    
+}
